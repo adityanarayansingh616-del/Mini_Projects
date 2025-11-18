6915400500b3a347a4eb7bac6be3e1657d6446bc
Aditya Narayan Singh <adityanarayansingh616@gmail.com>
2025-11-18 21:50:59 +0530

    Small change

---
+++ firstdict.py
@@ -0,0 +1,9 @@
+d={}
+print("Enter details of 5 students:")
+for i in range(5):
+    print("Student",i+1)
+    r=int(input("Enter roll no:"))
+    n=input("Enter name:")
+    m=float(input("Enter marks:"))
+    d[r]=[n,m]
+print(d)
