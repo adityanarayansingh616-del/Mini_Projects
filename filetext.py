e7fab0d3e50f805b7f9ec1e425c389d8df3b7a0e
Aditya Narayan Singh <adityanarayansingh616@gmail.com>
2026-05-05 19:20:19 +0530

    Added new code!

---
+++ filetext.py
@@ -0,0 +1,17 @@
+f=open("File","w+")
+def addlines():
+     l=list()
+     print("Enter 5 lines:")
+     for i in range(5):
+         line=input()+"\n"
+         l.append(line)
+     f.writelines(l)
+def func():
+      f.seek(0)
+      lines=f.readlines()
+      for line in lines:
+          if len(line.split())>=4:
+              print(line,end=str())
+addlines()
+func()
+f.close()
