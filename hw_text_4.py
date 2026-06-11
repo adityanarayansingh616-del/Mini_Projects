65d8f797840158e7226c9be32d52ed88eabdf6e3
Aditya Narayan Singh <adityanarayansingh616@gmail.com>
2026-06-11 18:15:35 +0530

    Added new code!

---
+++ hw_text_4.py
@@ -0,0 +1,19 @@
+cf=open("copyfile","w")
+f=open("File","a+")
+def add():
+    lines=[]
+    n=int(input("Enter no.of lines:"))
+    for i in range(n):
+        line=input("Enter a line:")
+        lines.append(line+"\n")
+    f.writelines(lines)
+def rem():
+    f.seek(0)
+    lines=f.readlines()
+    for line in lines:
+        if "a" in line:
+            cf.write(line)
+add()
+rem()
+f.close()
+cf.close()
