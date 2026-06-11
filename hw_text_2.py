7b183f0ae373187d8df3562456b1a6b2caaa3514
Aditya Narayan Singh <adityanarayansingh616@gmail.com>
2026-06-11 17:45:54 +0530

    Added new code!

---
+++ hw_text_2.py
@@ -0,0 +1,21 @@
+f=open("jvm","a+")
+def Add():
+    lines=[]
+    n=int(input("Enter no.of lines:"))
+    for i in range(n):
+        line=input("Enter a line:")
+        lines.append(line+"\n")
+    f.writelines(lines)
+def words():
+    f.seek(0)
+    lines=f.readlines()
+    for line in lines:
+        c=0
+        words=line.split()
+        for word in words:
+            if len(word)>4:
+                c+=1
+        print(c)
+Add()
+words()
+f.close()
