08a4a4263bf3b77aaf674a17039fd342b4e081af
Aditya Narayan Singh <adityanarayansingh616@gmail.com>
2026-06-12 20:42:56 +0530

    Added new code!

---
+++ hw_binary_2.py
@@ -0,0 +1,23 @@
+import pickle
+f=open("filebinary","wb+")
+def add():
+    n=int(input("Enter no.of records:"))
+    for i in range(n):
+        l=list()
+        roll=int(input("Enter roll:"))
+        name=input("Enter name:")
+        marks=float(input("Enter marks:"))
+        d={roll:[name,marks]}
+        l.append(d)
+    pickle.dump(l,f)
+def search():
+    f.seek(0)
+    rollin=int(input("Enter roll to search:"))
+    data=pickle.load(f)
+    for dic in data:
+        for key in dic:
+            if key==rollin:
+                print(dic[key][0])
+add()
+search()
+#Can also be done by dumping dicts one by one instead of one go in the form of list containing dicts.
