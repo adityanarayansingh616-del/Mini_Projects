076aeb43395802e202e1710699c180deb4d7307d
Aditya Narayan Singh <adityanarayansingh616@gmail.com>
2026-06-23 15:19:07 +0530

    Added new code!

---
+++ search_record.py
@@ -0,0 +1,24 @@
+import pickle
+f=open("filebinary","wb")
+n=int(input("Enter no.of students:"))
+for i in range(n):
+    name=input("Enter name:")
+    roll=int(input("Enter roll:"))
+    marks=float(input("Enter marks:"))
+    pickle.dump([roll,name,marks],f)
+f.close()
+def search():
+    x=0
+    with open("filebinary","rb") as f:
+        while True:
+            try:
+                data=pickle.load(f)
+                if data[2]>90:
+                    print("Name:",data[1])
+                    print("Roll number:",data[0])
+                    x=1
+            except EOFError:
+                break
+    if x==0:
+        print("No data found!")
+search()
