b5555092115d213fbf31c0d9125c803959894645
Aditya Narayan Singh <adityanarayansingh616@gmail.com>
2026-08-11 22:23:46 +0530

    Organized files!

---
+++ Class 11 and 12 CBSE/My_Data.py
@@ -0,0 +1,3 @@
+#To store personal student information in a dictionary.
+Aditya={"Name":'Aditya Narayan Singh',"Class":11,"School":"JVM Shyamali, Ranchi",
+    "Favourite subject":"Computer Science and AI"}
